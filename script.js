615996d1fc65d923fd08c341f2c2f82cb832f9a5
sebastianCain <sebi.cain@yahoo.com>
2015-01-30 01:55:08 -0500

    structure set up

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 jQuery(document).ready(function(){
-
-	var page2 = jQuery('#page2').offset().top;
+    var page2 = jQuery('#page2').offset().top;
 	jQuery('#scroll').click(function(){
 		jQuery('html, body').animate({scrollTop:page2}, 'slow');
+		return false;
+	});
+    var page3 = jQuery('#page3').offset().top;
+	jQuery('#scroll2').click(function(){
+		jQuery('html, body').animate({scrollTop:page3}, 'slow');
+		return false;
+	});
+    var page4 = jQuery('#page4').offset().top;
+	jQuery('#scroll3').click(function(){
+		jQuery('html, body').animate({scrollTop:page4}, 'slow');
 		return false;
 	});
 });
